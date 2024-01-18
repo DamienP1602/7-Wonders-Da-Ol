61211b78bf2ebf0710358a21f4f8ca1c7486e38f
DamienP1602 <148540742+DamienP1602@users.noreply.github.com>
2024-01-18 15:27:13 +0100

    Add files via upload

--- 7_Wonders/MovementComponent.h
+++ 7_Wonders/MovementComponent.h
@@ -2,15 +2,21 @@
 
 enum DirectionData
 {
-    DIRECTION_NONE, DIRECTION_RIGT, DIRECTION_LEFT, DIRECTION_UP, DIRECTION_DOWN
+	DIRECTION_NONE, DIRECTION_RIGT, DIRECTION_LEFT, DIRECTION_UP, DIRECTION_DOWN
 };
 
 class MovementComponent
 {
-    float speed; //pour l'animation
-    DirectionData direction;
-    bool canMove;
+	float speed; //pour l'animation
+	DirectionData direction;
+	bool canMove;
 
 public:
-    MovementComponent();
+	MovementComponent();
+
+	void ToggleMove()
+	{
+		canMove = !canMove;
+	}
 };
+
