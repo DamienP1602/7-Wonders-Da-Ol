db7c42913b1abb57ab8c6164458014c2c16f739b
OlivierM98 <148540754+OlivierM98@users.noreply.github.com>
2024-01-18 17:02:48 +0100

    lastUpdate

--- 7_Wonders/MovementComponent.cpp
+++ 7_Wonders/MovementComponent.cpp
@@ -6,3 +6,17 @@ MovementComponent::MovementComponent()
 	direction = DIRECTION_NONE;
 	canMove = false;
 }
+
+void MovementComponent::Move(CollisionComponent* _collision, Entity* _entity)
+{
+	if (!canMove) return;
+
+	TryToMove(_collision, _entity, direction);
+}
+
+void MovementComponent::TryToMove(CollisionComponent* _collision, Entity* _entity, const DirectionData& _direction)
+{
+	const float _destination = _direction * speed;
+
+
+}
--- 7_Wonders/MovementComponent.h
+++ 7_Wonders/MovementComponent.h
@@ -1,8 +1,22 @@
 #pragma once
+#include "CollisionComponent.h"
 
-enum DirectionData
+enum DirectionType
 {
 	DIRECTION_NONE, DIRECTION_RIGT, DIRECTION_LEFT, DIRECTION_UP, DIRECTION_DOWN
+};
+
+struct DirectionData
+{
+	DirectionType type;
+	Vector2f direction;
+
+	DirectionData()
+	{
+		type = DIRECTION_NONE;
+		direction = Vector2f();
+	}
+	DirectionData();
 };
 
 class MovementComponent
@@ -14,6 +28,9 @@ class MovementComponent
 public:
 	MovementComponent();
 
+public:
+	void Move(CollisionComponent* _collision, Entity* _entity);
+	void TryToMove(CollisionComponent* _collision, Entity* _entity, const DirectionData& _direction);
 	void ToggleMove()
 	{
 		canMove = !canMove;
